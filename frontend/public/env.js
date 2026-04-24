af48ce40456fdc49d963e6e64e06bffd79148fcf
POLPAMPLONA <157718136+POLPAMPLONA@users.noreply.github.com>
2026-04-24 16:10:24 +0200

    Delete frontend/public/env.js

--- frontend/public/env.js
+++
@@ -1,5 +0,0 @@
-window.ENV = {
-  FAVORITES: "https://service-favorites-65uh.onrender.com/",
-  COMMENTS: "https://service-comments.onrender.com/",
-  HISTORY: "https://service-history.onrender.com/"
-};
